bdd5bdabea92337ae6f25f0955fc8f98706172c0
BrunoVieira003 <76965972+BrunoVieira003@users.noreply.github.com>
2024-06-11 19:41:36 -0300

    Add telas de CRUD de produtos

--- src/componentes/produtos/listaProdutos.tsx
+++ src/componentes/produtos/listaProdutos.tsx
@@ -1,22 +1,38 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import { Component } from "react";
+import { Component, ReactNode } from "react";
+import FormularioAtualizarProduto from "./formularioAtualizarProduto";
+import FormularioCadastroProduto from "./formularioCadastroProduto";
 
 type props = {
     tema: string
 }
 
-export default class ListaProduto extends Component<props>{
+export default class ListaProdutos extends Component<props>{
     render() {
         let tema = this.props.tema
+        let items: Array<ReactNode> = []
+        for(let i = 1; i < 7; i++){
+            let itemData = {
+                id: i,
+                nome: `Produto ${i}`,
+                preco: `R$ ${i}${i},${i}0`
+            }
+            items.push(
+                <div className="list-group-item d-flex justify-content-between align-items-center">
+                    <div className="fw-bold">{itemData.nome}</div>
+                    <div className="ms-2">
+                        <button type="button" className="btn btn-warning mb-4 mt-3 mx-1" data-bs-toggle="modal" data-bs-target={`#atualizarProduto${i}`}>Atualizar</button>
+                        <button type="button" className="btn btn-danger mb-4 mt-3 mx-1">Excluir</button>
+                        <FormularioAtualizarProduto tema={tema} id={i} nome={itemData.nome} preco={itemData.preco}/>
+                    </div>
+                </div>
+            )
+        }
         return (
             <div className="container-fluid">
+                <button type="button" className="btn btn-primary mb-4 mt-3" data-bs-toggle="modal" data-bs-target="#cadastroProduto">Cadastrar novo produto</button>
+                <FormularioCadastroProduto tema={tema}/>
                 <div className="list-group">
-                    <a href="#" className="list-group-item list-group-item-action">Produto 1</a>
-                    <a href="#" className="list-group-item list-group-item-action">Produto 2</a>
-                    <a href="#" className="list-group-item list-group-item-action" style={{ backgroundColor: tema }} >Produto 3</a>
-                    <a href="#" className="list-group-item list-group-item-action">Produto 4</a>
-                    <a href="#" className="list-group-item list-group-item-action">Produto 5</a>
-                    <a href="#" className="list-group-item list-group-item-action">Produto 6</a>
+                    {items}
                 </div>
             </div>
         )
