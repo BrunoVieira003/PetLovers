cb1496653967aab5c5a135166441b5412d09bf49
BrunoVieira003 <76965972+BrunoVieira003@users.noreply.github.com>
2024-06-11 18:38:41 -0300

    Exibe telefones e rgs dos clientes

--- src/componentes/clientes/listaClientes.tsx
+++ src/componentes/clientes/listaClientes.tsx
@@ -16,7 +16,37 @@ export default class ListaCliente extends Component<props>{
                     <div className="ms-2 me-auto">
                     <div className="fw-bold">Cliente {i}</div>
                         cliente0{i}@gmail.com
+                    <div className="collapse" id={`telefones${i}`}>
+                        <div className="card card-body">
+                            <h4>Telefones</h4>
+                            <ul className="list-group">
+                                <li className="list-group-item">(12){i} 23456789</li>
+                                <li className="list-group-item">(21){i} 2345678{i}</li>
+                            </ul>
+                        </div>
                     </div>
+                    <div className="collapse" id={`rgs${i}`}>
+                        <div className="card card-body">
+                            <h4>RGs</h4>
+                            <ul className="list-group">
+                                <li className="list-group-item">0{i} 234 567-8</li>
+                                <li className="list-group-item">{i}{i} 234 567-8</li>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="btn-group">
+                        <button className="btn btn-outline-primary" type="button" data-bs-toggle="collapse" data-bs-target={`#telefones${i}`} aria-expanded="false" aria-controls="telefones">
+                            Ver telefones
+                        </button>
+                        <button className="btn btn-outline-primary" type="button" data-bs-toggle="collapse" data-bs-target={`#rgs${i}`} aria-expanded="false" aria-controls="rgs">
+                            Ver RGs
+                        </button>
+                    </div>
+                    
+
+
+
                 </li>
             )
         }
