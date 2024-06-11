aa5ec3169b95c71a8c345279ae99f86a5419de9a
BrunoVieira003 <76965972+BrunoVieira003@users.noreply.github.com>
2024-06-11 18:03:08 -0300

    Add componentes de input

--- src/componentes/pets/formularioCadastroPet.tsx
+++ src/componentes/pets/formularioCadastroPet.tsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import Modal from "../modal";
+import TextInput from "../form/TextInput";
 
 type props = {
     tema: string
@@ -13,23 +14,14 @@ export default class FormularioCadastroPet extends Component<props> {
             <div className="container-fluid">
                 <Modal title="Cadastro de pet" id="cadastroPet">
                     <form>
-                        <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" />
-                        </div>
-                        <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Tipo" aria-label="Tipo" aria-describedby="basic-addon1" />
-                        </div>
-                        <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Raça" aria-label="Raça" aria-describedby="basic-addon1" />
-                        </div>
-                        <select className="form-select" aria-label="Gênero">
+                        <TextInput label="Nome" placeholder="Ex: Double Tó, Café, etc" id="nome"/>
+                        <TextInput label="Tipo" placeholder="Ex: Cachorro, Gato, etc" id="tipo"/>
+                        <TextInput label="Raça" placeholder="Ex: Siamês, Pinscher, etc" id="raca"/>
+                        <select className="form-select mb-3" aria-label="Gênero">
                             <option selected>Escolha o gênero do pet</option>
                             <option value="m">Macho</option>
                             <option value="f">Fêmea</option>
                         </select>
-                        <div className="input-group mb-3">
-                            <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
-                        </div>
                     </form>
                 </Modal>
             </div>
