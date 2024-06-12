db37bb4968291edc8a05adbdbbfd6b09ca92993b
Bruno Vieira <brunosilverio01@gmail.com>
2024-06-12 10:32:17 -0300

    Add ranking MaisConsumidos

--- src/componentes/rankings/RankingsHome.tsx
+++ src/componentes/rankings/RankingsHome.tsx
@@ -1,6 +1,7 @@
 import {ReactNode, Component} from 'react'
 import ClientePorConsumo from './ClientePorConsumo'
 import ClientePorValor from './ClientePorValor'
+import MaisConsumidos from './MaisConsumidos'
 
 type props = {
     tema: string
@@ -10,9 +11,10 @@ export default class RankingsHome extends Component<props>{
     render(): ReactNode {
         const { tema } = this.props
         return (
-            <div className="container-fluid d-flex">
+            <div className="container-fluid d-flex flex-wrap flex-lg-nowrap">
                 <ClientePorConsumo tema={tema}/>
                 <ClientePorValor tema={tema}/>
+                <MaisConsumidos tema={tema}/>
             </div>
         )
     }
